4e8b9cc5a9d54f975e94bd81e9071c397c8ab58f
PawsOfDestruction <purvanjaro2@gmail.com>
2023-12-16 21:39:10 +0200

    pretty-print and fix logging

--- main.go
+++ main.go
@@ -6,20 +6,22 @@ import (
 	"log"
 	"os"
 	"path/filepath"
+	"time"
 
 	"github.com/NublyBR/go-vpk"
 )
 
 func main() {
-    rootPath := `E:\SteamLibrary\steamapps\common\Team Fortress 2\tf\tf2_misc_dir.vpk`	
-	logfile, err  := os.Create("write.log")
+    rootPath := `E:\SteamLibrary\steamapps\common\Team Fortress 2\tf\tf2_sound_misc_dir.vpk`	
+	logfile, err  := os.Create("vpk-extractor-"+time.Now().Format("2006-01-02-15-04-05")+".log")
 
+	logger := log.New(os.Stdout, "INFO: ", log.Ldate|log.Ltime)
 
 	defer logfile.Close()
-	log.SetOutput(logfile)
+	logger.SetOutput(logfile)
 
 	if err != nil {
-		log.Fatal(err)
+		logger.Fatal(err)
 	}
 
 	pak, err := vpk.OpenDir(rootPath)
@@ -30,7 +32,7 @@ func main() {
 	for _, file := range pak.Entries() {
 		entry, err := file.Open()
 		if err != nil {
-			log.Fatal(err)
+			logger.Fatal(err)
 		}
 		
 		path := filepath.Join("output",file.Filename())
@@ -38,24 +40,25 @@ func main() {
 		// Ensure the directories exist by using os.MkdirAll
 		dir := filepath.Dir(path)
 		if dir_err := os.MkdirAll(dir, 0755); dir_err != nil {
-			log.Fatal(dir_err)
+			logger.Fatal(dir_err)
 		}
-
-		err, fileBytes := WriteVpkFile(entry, path)
-		if err != nil {
-			log.Fatal(err)
+		logger.Println("Writing file",path)
+		fmt.Println("Writing file",path)
+		writeErr := WriteVpkFile(entry, path)
+		if writeErr != nil {
+			logger.Fatal(err)
 		}	
-		fmt.Println(fileBytes)
 	}
-	
+	logger.Println("Successfully extracted content to "+rootPath)
+	fmt.Println("Successfully extracted content to "+rootPath)
 }
 
 
 
-func WriteVpkFile(file vpk.FileReader, path string) (error, bool) {
+func WriteVpkFile(file vpk.FileReader, path string) error {
 	f, err := os.Create(path)
 	if err != nil {
-		return err, false
+		return err
 	}
 
     for {
@@ -70,11 +73,11 @@ func WriteVpkFile(file vpk.FileReader, path string) (error, bool) {
 		}
 		if n > 0 {
 			content := buf[:n]
-			fmt.Println("Buf: ",string(content))
-			fmt.Println("Bytes: ",content)
+			// Print the actual content of the file, TODO: uncomment this when we add a --verbose flag
+			// fmt.Println("Buf: ",string(content))
 			f.Write(content)
 		}
     }
 	f.Sync()
-	return nil, true
+	return nil
 }
